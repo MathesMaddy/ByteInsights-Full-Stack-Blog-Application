01dad9a08100767356bc782578000c1da9664631
Matheswaran <138348719+MathesMaddy@users.noreply.github.com>
2025-01-06 14:40:58 +0530

    Update CorsContorller.js

--- backend/controller/CorsContorller.js
+++ backend/controller/CorsContorller.js
@@ -1,11 +1,12 @@
 const whiteList = [ 
-    'https://byteinsights-frontend.onrender.com/', 
+    'https://byteinsights-frontend.onrender.com', 
     'http://192.168.43.154:5173',
 ];
 
 const websiteAllow = {    
         
     origin: (origin, callback) => {
+        console.log("Request origin "+origin);
         if (whiteList.indexOf(origin) !== -1 || !origin) {            
             callback(null, true);
         } 
