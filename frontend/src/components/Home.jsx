c9d7b803da28fa4080de064fb26d889b3c129019
Matheswaran <138348719+MathesMaddy@users.noreply.github.com>
2025-01-09 19:44:37 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,20 +2,30 @@ import React, { useEffect, useState } from 'react'
 import { Posts } from './Posts'
 
 export const Home = () => {
-  
-    const [posts,SetPosts] = useState([]);
+    
+    const [posts,SetPosts] = useState([]);   
 
     const url = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/posts`;
 
     useEffect(() => {
       fetch( url )
         .then( res => { 
-          res.json() .then( post => { 
-            SetPosts(post)})})
-    },[])
+          res.json() 
+        .then( post => { 
+            SetPosts(post)           
+          })
+        }).catch(e) {
+      console.log(e);
+      }
+    }, [])
 
     return (
-      <>        
+      <> 
+        { !post.length && (
+          <div className = "loading-posts">
+            <h2> Server has been Restarted. Blog Post's Loading within 50 Seconds. </h2>
+          </div>
+        )}
         { posts.length > 0 && posts.map( post => (
           <Posts {...post} key={post._id} />
         ))
